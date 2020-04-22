c28f3c897a683aab43db31e21780ba6d7eb3fb9b
javitocor <javitocor@gmail.com>
2020-04-22 11:02:45 +0200

    Added user model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 15 }, uniqueness: true
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP },
+                      uniqueness: true
+    validates :password, presence: true, length: { minimum:3, maximum:20 }
+end
